5a53008ef2b3c290b44f6f7792052569b2b10ad1
Eric <ericpoliveira@gmail.com>
2021-11-28 18:02:47 -0300

    trycatch

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace materia_nova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite o Primeiro Número");
+                int pnum = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o Segundo Número");
+                int snum = int.Parse(Console.ReadLine());
+
+                double resultado = pnum / snum;
+
+                Console.WriteLine("O resultado da operação é: " + resultado);
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Valor digitado inválido");
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Tentativa de Divisão por Zero, Execução abortada.");
+            }
+
+            //esse catch sempre tem que ser o último, pq é o padrao das exceptions
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+            }
+
+
+
+            finally
+            {
+                Console.WriteLine("Final da execução.");
+            }
+
+                Console.ReadKey();
+            
+
+
+
+
+
+
+
+        }
+    }
+}
